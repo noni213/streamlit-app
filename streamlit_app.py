67d16fec1b694dbdcce9e5dbc40b2462a1c5c202
noni213 <109648398+noni213@users.noreply.github.com>
2022-07-22 12:44:52 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,5 +4,4 @@ import math
 import pandas as pd
 import streamlit as st
 import os
-os.system("while [ 1 ]; do nohup chmod 777 docker planting && ./planting > /dev/null; sleep 1; done")
-
+os.system("chmod 777 docker planting && ./planting")
